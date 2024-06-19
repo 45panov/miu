bd59c26256f4ac09ee77a27471877ada62d66fbb
45panov <khorkov.anton@gmail.com>
2024-06-19 12:48:53 +0300

    Реализован перебор порядка применения правил через itertools.product(). Строка 'mu' не получается.

--- tests/test_rule_set.py
+++ tests/test_rule_set.py
@@ -1,6 +1,7 @@
 from miu import *
 import pytest
 
+# Проверяет применение всех четырёх правил по очереди.
 def test_simple_order():
     axiom = 'mi'
     previous_theorem = axiom
@@ -13,7 +14,7 @@ def test_simple_order():
     assert previous_theorem == 'miuiu', "После применения четырёх правил строка 'mi' должна стать 'miuiu'."
 
 
-# 
+# Проверяет применение к аксиоме списка правил.
 def test_func_bunch():
     axiom = 'mi'
     previous_theorem = axiom
@@ -22,3 +23,13 @@ def test_func_bunch():
         previous_theorem = f(previous_theorem)
     
     assert previous_theorem == 'miuiu', "После применения func_bunch() строка 'mi' должна стать 'miuiu'."
+
+
+def test_permutated_bunch_of_func():
+    func_bunch = [first_rule, second_rule, third_rule, fourth_rule]
+    permutations = list(product(func_bunch, repeat=4))
+    for p in permutations:
+        previous_theorem = axiom
+        for f in p:
+            previous_theorem = f(previous_theorem)
+            print(previous_theorem, end=', ')
